75521ad934b58c4f9eb6451ad2de30e16ff33d8d
tuyechiev <khaldybek.tuyechiyev@narxoz.kz>
2025-02-18 12:37:14 +0500

    [task 2]: create user module

--- src/Components/Moderator/CourseOnePage.jsx
+++ src/Components/Moderator/CourseOnePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { Card, Button, Typography, List, Descriptions, Spin, FloatButton, Modal, Popconfirm, notification } from 'antd';
 import { PlusSquareOutlined, DeleteOutlined } from "@ant-design/icons";
